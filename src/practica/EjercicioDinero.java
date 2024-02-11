d2f4d0438577a576dc30eedfd28af298cbebc7d5
David-alzate <alzatedavid0126@gmail.com>
2024-02-10 19:36:24 -0500

    Total dolares

--- src/practica/EjercicioDinero.java
+++ src/practica/EjercicioDinero.java
@@ -8,18 +8,19 @@ public class EjercicioDinero {
 		float Guillermo;
 		float Luis;
 		float Juan;
+		float total;
 		
 		Scanner entrada = new Scanner(System.in);
 		System.out.print("Ingrese el N de dolares de Guillermo");
 		Guillermo = entrada.nextFloat();
 		Luis = (Guillermo / 2);
 		Juan = ((Guillermo + Luis) / 2);
+		total = (Guillermo + Juan + Luis);
 		
 		System.out.println("El N de dolares de Guillermo es: " + Guillermo);
 		System.out.println("El N de dolares de Luis es: " + Luis);
 		System.out.println("El N de dolares de Juan es: " + Juan);
-		
-		
+		System.out.print("El total de dolares es: " + total);		
 		entrada.close();
 	}
 }
