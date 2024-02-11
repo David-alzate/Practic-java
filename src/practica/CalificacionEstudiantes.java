be74a743e12f475fcc90a03eab550284f25e0ffa
David-alzate <alzatedavid0126@gmail.com>
2024-02-10 20:12:00 -0500

    Nota final

--- src/practica/CalificacionEstudiantes.java
+++ src/practica/CalificacionEstudiantes.java
@@ -9,17 +9,27 @@ public class CalificacionEstudiantes {
 		float primerParcial;
 		float segundoParcial;
 		float examenFinal;
+		float calificacionFinal;
 		
 		Scanner entrada = new Scanner(System.in);
-		System.out.print("Digite La calificacion de participacion");
-		participacion = entrada.nextInt();
-		System.out.print("Digite La calificacion del primer parcial");
-		primerParcial = entrada.nextInt();
-		System.out.print("Digite La calificacion del segundo parcial ");
-		segundoParcial = entrada.nextInt();
-		System.out.print("Digite La calificacion del examen final");
-		examenFinal = entrada.nextInt();
-
+		System.out.print("Digite La calificacion de participacion: ");
+		participacion = entrada.nextFloat();
+		System.out.print("Digite La calificacion del primer parcial: ");
+		primerParcial = entrada.nextFloat();
+		System.out.print("Digite La calificacion del segundo parcial: ");
+		segundoParcial = entrada.nextFloat();
+		System.out.print("Digite La calificacion del examen final: ");
+		examenFinal = entrada.nextFloat();
+		
+		participacion *= 0.10f;
+		primerParcial *= 0.25f;
+		segundoParcial *= 0.25f;
+		examenFinal *= 0.40f;
+		
+		calificacionFinal = (participacion + primerParcial + segundoParcial + examenFinal);
+		System.out.print("La nota final es: " + calificacionFinal);
+		
+		entrada.close();
 	}
 
 }
