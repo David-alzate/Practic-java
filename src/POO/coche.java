857663bb391b8d084c08b8c98e7cc833ac07695d
David-alzate <alzatedavid0126@gmail.com>
2024-02-15 18:29:01 -0500

    Atributos

---
+++ src/POO/coche.java
@@ -0,0 +1,31 @@
+package POO;
+
+public class coche {
+	//Atributos
+	String color;
+	String marca;
+	int km;
+	
+	// Metodo
+	public static void main(String[] args) {
+		coche coche1 = new coche();
+		
+		coche1.color = "Blanco";
+		coche1.marca = "Audi";
+		coche1.km = 0;
+		
+		System.out.println("El color del coche 1 es: "+ coche1.color);
+		System.out.println("La marca del coche 1 es: "+ coche1.marca);
+		System.out.println("El km del coche 1 es: "+ coche1.km);
+		
+		coche Coche2 = new coche();
+		
+		Coche2.color = "Negro";
+		Coche2.marca = "BMW";
+		Coche2.km = 1200;
+		
+		System.out.println("El color del coche 1 es: "+ Coche2.color);
+		System.out.println("La marca del coche 1 es: "+ Coche2.marca);
+		System.out.println("El km del coche 1 es: "+ Coche2.km);
+	}
+}
