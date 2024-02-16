100aee9bfc211cf9539e0122cec71d523c0f4438
David-alzate <alzatedavid0126@gmail.com>
2024-02-16 11:29:52 -0500

    sobrecarga de metodos

--- src/POO/mainPersona.java
+++ src/POO/mainPersona.java
@@ -4,8 +4,10 @@ public class mainPersona {
 
 	public static void main(String[] args) {
 		persona p1 = new persona("Alejandro", 21);
+		
 
 		p1.mostrarDatos();
+		p1.mostrarDatos("Alzate");
 
 	}
 }
