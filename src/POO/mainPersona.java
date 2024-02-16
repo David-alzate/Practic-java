2194300745379e9e848eac4f130b1038c366140d
David-alzate <alzatedavid0126@gmail.com>
2024-02-16 11:18:28 -0500

    Persona

---
+++ src/POO/mainPersona.java
@@ -0,0 +1,11 @@
+package POO;
+
+public class mainPersona {
+
+	public static void main(String[] args) {
+		persona p1 = new persona("Alejandro", 21);
+
+		p1.mostrarDatos();
+
+	}
+}
---
+++ src/POO/persona.java
@@ -0,0 +1,19 @@
+package POO;
+
+public class persona {
+	String nombre;
+	int edad;
+
+	// Metodo constructor
+	public persona(String _nombre, int _edad) {
+		nombre = _nombre;
+		edad = _edad;
+
+	}
+	
+	public void mostrarDatos() {
+		System.out.println("El nombre es: " + nombre);
+		System.out.println("La edad es: "+ edad);
+	}
+
+}
