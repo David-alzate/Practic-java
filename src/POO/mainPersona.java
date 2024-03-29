7d022ae6f694a0d60c0f18f084491fd9aab29121
David-alzate <alzatedavid0126@gmail.com>
2024-03-29 15:42:18 -0500

    Revert "sobrecarga de metodos"
    This reverts commit 100aee9bfc211cf9539e0122cec71d523c0f4438.

--- src/POO/mainPersona.java
+++ src/POO/mainPersona.java
@@ -4,10 +4,8 @@ public class mainPersona {
 
 	public static void main(String[] args) {
 		persona p1 = new persona("Alejandro", 21);
-		
 
 		p1.mostrarDatos();
-		p1.mostrarDatos("Alzate");
 
 	}
 }
--- src/POO/persona.java
+++ src/POO/persona.java
@@ -6,22 +6,14 @@ public class persona {
 
 	// Metodo constructor
 	public persona(String nombre, int edad) {
-		super();
 		this.nombre = nombre;
 		this.edad = edad;
+
 	}
 
 	public void mostrarDatos() {
 		System.out.println("El nombre es: " + nombre);
 		System.out.println("La edad es: " + edad);
 	}
-	
-	public void mostrarDatos(String apellido) {
-		System.out.println(apellido);
-		System.out.println("El nombre es: " + nombre);
-		System.out.println("La edad es: " + edad);
-	}
-
-
 
 }
